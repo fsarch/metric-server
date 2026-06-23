06d80b262bd9533e3bb8d8015a02ce20c8662219
Michael Braun <michael@web4brauns.de>
2026-06-23 22:28:08 +0200

    refactor: remove unused configuration and database module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ControllersModule } from './controllers/controllers.module.js';
-import configuration from './configuration.js';
-import { DATABASE_OPTIONS } from './database/index.js';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [configuration],
-    }),
-    TypeOrmModule.forRoot({
-      ...DATABASE_OPTIONS,
-      type: 'postgres',
-      autoLoadEntities: true,
-    }),
     ControllersModule,
   ],
   controllers: [],
