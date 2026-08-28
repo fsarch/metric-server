cb6355c73b0b78270853a2edd65b6075fd8ac8e5
Michael Braun <michael@web4brauns.de>
2026-08-28 22:30:42 +0200

    Upgrade dependencies to latest versions, including NestJS to v12.0.1 and @fsarch/server to v0.2.0 in package-lock.json.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { AppModule } from './app.module.js';
 import { FsArchAppBuilder } from '@fsarch/server';
 import { DATABASE_OPTIONS } from './database/index.js';
+import { Role } from './constants/role.enum.js';
 
 async function bootstrap() {
   const app = await new FsArchAppBuilder(AppModule, {
@@ -13,6 +14,7 @@ async function bootstrap() {
       version: '1.0',
     })
     .enableAuth()
+    .enableUac(Object.values(Role))
     .enableSoftDeletion()
     .setDatabase(DATABASE_OPTIONS)
     .build();
